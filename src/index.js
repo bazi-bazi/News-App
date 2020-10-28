0f23a5dc6c80901f2233ba97c1408cee4ad99b3c
bazi <vazha.davituliani@gmail.com>
2020-10-28 17:30:07 +0400

    initial new firebase and redux

--- src/index.js
+++ src/index.js
@@ -1,13 +1,59 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import { createStore, applyMiddleware, compose } from "redux";
+import { Provider } from "react-redux";
+import { ReactReduxFirebaseProvider, getFirebase } from "react-redux-firebase";
+import { createFirestoreInstance, getFirestore } from "redux-firestore";
+import { rootReducer } from "./redux/reducers/rootReducer";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
+import { logger } from "redux-logger";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from 'redux-thunk'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBYxODoEH_hhq0fVc7AAqk1sj6ou04jJ4A",
+  authDomain: "news-app-5970e.firebaseapp.com",
+  databaseURL: "https://news-app-5970e.firebaseio.com",
+  projectId: "news-app-5970e",
+  storageBucket: "news-app-5970e.appspot.com",
+  messagingSenderId: "389602304910",
+  appId: "1:389602304910:web:15da7bdb84ef0ed3807adf"
+};
+
+const rrfConfig = {
+  userProfile: "users",
+  useFirestoreForProfile: true,
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.firestore();
+
+const initialState = {};
+const store = createStore(rootReducer, initialState, composeWithDevTools(applyMiddleware(logger, thunk.withExtraArgument({getFirebase, getFirestore})))
+);
+
+const rrfProps = {
+  firebase,
+  config: rrfConfig,
+  dispatch: store.dispatch,
+  createFirestoreInstance, //since we are using Firestore
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <ReactReduxFirebaseProvider {...rrfProps}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ReactReduxFirebaseProvider>
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
 
---
+++ src/redux/reducers/authReducer.js
@@ -0,0 +1,18 @@
+// import { toast } from "react-toastify";
+
+// const authReducer=( state = {}, action) => {
+//   switch(action.type) {
+//     case "SIGN_IN":
+//       toast("Welcome back..");
+//       return state;
+//     case "SIGN_IN_ERR":
+//       toast.error("Sign in error...");
+//       return state;
+//     default:
+//       return state;
+
+
+//   }
+// }
+
+// export default authReducer;
